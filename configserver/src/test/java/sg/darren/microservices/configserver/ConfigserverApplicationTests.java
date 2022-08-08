4599b667021e0cd92a8dc4372a90d516bb15f2f0
Xue Yu <xueyublue@gmail.com>
2022-08-08 08:43:42 +0800

    Initial Commit - Config Server

---
+++ configserver/src/test/java/sg/darren/microservices/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.darren.microservices.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
