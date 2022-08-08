f318a23bba60819b3126999fc259b2a5b154d41a
Xue Yu <xueyublue@gmail.com>
2022-08-08 19:50:01 +0800

    Updated account service to allow refresh configurations

--- accounts/src/main/java/sg/darren/microservices/accounts/AccountsApplication.java
+++ accounts/src/main/java/sg/darren/microservices/accounts/AccountsApplication.java
@@ -2,8 +2,10 @@ package sg.darren.microservices.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/sg/darren/microservices/accounts/controller/AccountsController.java
+++ accounts/src/main/java/sg/darren/microservices/accounts/controller/AccountsController.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sg.darren.microservices.accounts.config.AccountsServiceConfig;
 import sg.darren.microservices.accounts.model.Account;
 import sg.darren.microservices.accounts.model.Customer;
@@ -18,12 +15,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/accounts")
 public class AccountsController {
 
     private final AccountRepository accountRepository;
     private final AccountsServiceConfig accountsServiceConfig;
 
-    @PostMapping("/accounts")
+    @PostMapping
     public List<Account> getAccountList(@RequestBody Customer customer) {
         return accountRepository.findByCustomerId(customer.getCustomerId());
     }
