00791547fb5428ece0b6fcc612ffe4cb8d872305
Xue Yu <xueyublue@gmail.com>
2022-08-12 17:51:51 +0800

    Added custom routes

--- gatewayserver/src/main/java/sg/darren/microservices/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/sg/darren/microservices/gatewayserver/GatewayServerApplication.java
@@ -2,14 +2,39 @@ package sg.darren.microservices.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/retailbank/accounts/**")
+                        .filters(f -> f.rewritePath("/retailbank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://ACCOUNTS")).
+                route(p -> p
+                        .path("/retailbank/loans/**")
+                        .filters(f -> f.rewritePath("/retailbank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://LOANS")).
+                route(p -> p
+                        .path("/retailbank/cards/**")
+                        .filters(f -> f.rewritePath("/retailbank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://CARDS")).build();
+    }
 
 }
